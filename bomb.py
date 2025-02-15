0542b468c97b9e20019ab372dbe838f55928cdcb
Mamiozp <mamitablet21@gmail.com>
2025-02-16 02:06:36 +0300

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -34,7 +34,7 @@ while 1:
 ███████▄░░░░░░░░░░░▄███████
 ██████████▄▄▄▄▄▄▄██████████
 ███████████████████████████    
-    Sms: {}           {}by {}@mamiozp\n  
+    Sms: {}           {}by {}@m_ozpnar\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
